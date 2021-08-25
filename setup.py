99f5fdc6b82cdb19f56df9f05685a260e72c9d21
Connor Ryan <Connor.Ryan@Revature.net>
2021-08-24 23:21:24 -0400

    Added some basic CSS to the tableview to make it look slightly nicer.
    Signed-off-by: Connor Ryan <Connor.Ryan@Revature.net>

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-
-setup(name='DiceBot',
-      version='1.0',
-      description='Dice and DnD utilities',
-      author='Connor Ryan',
-      author_email='cryan2@upei.ca',
-      )
