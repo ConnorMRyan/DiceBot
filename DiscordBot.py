a33460caee8f5c2b1901e9ab0fcc61b3793725cf
Connor Ryan <Connor.Ryan@Revature.net>
2021-09-09 17:10:08 -0400

    Formating and quick changes.
    Signed-off-by: Connor Ryan <Connor.Ryan@Revature.net>

--- DiscordBot.py
+++ DiscordBot.py
@@ -41,12 +41,13 @@ async def roll_die(ctx, parsable_string: str):
 
 @bot.command(name='pokemon', help='Responds with a picture of a pokemon')
 async def pokemon(ctx, pokenum: str):
-    url = pb.SpriteResource('pokemon', pokenum, other=True, official_artwork=True).url
+    sprite = pb.SpriteResource('pokemon', pokenum, other=True, official_artwork=True)
+    print(sprite)
     try:
         pokenum = int(pokenum)
         if pokenum > 898 or pokenum < 0:
             pokenum = random.randint(1, 898)
-        await ctx.send(f"{url}")
+        await ctx.send(f"{sprite.url}")
     except ValueError:
         await ctx.send("```Silly, That's not a pokemon at all, Have a bre instead.\n```"
                        "https://raw.githubusercontent.com/"
